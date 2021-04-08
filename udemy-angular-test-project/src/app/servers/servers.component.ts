167464329c059b8b391839d63f784349d1c585ba
Attila Szabo <attila.szabo.vrc@gmail.com>
2021-04-08 16:15:32 +0200

    test project updated with directives and binding

--- udemy-angular-test-project/src/app/servers/servers.component.ts
+++ udemy-angular-test-project/src/app/servers/servers.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = "No server was created!";
+  serverName ="test server";
+  serverCreated= false;
+  servers = ["Testserver", "Testserver 2"];
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
 
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = "Server was created! Name is " + this.serverName;
+  }
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
+  }
 }
