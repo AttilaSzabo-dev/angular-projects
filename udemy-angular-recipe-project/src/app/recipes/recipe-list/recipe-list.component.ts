57b3769c30734c7eac1e733b6adde9315a74b181
Attila Szabo <attila.szabo.vrc@gmail.com>
2021-04-12 16:26:50 +0200

    components and databinding

--- udemy-angular-recipe-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ udemy-angular-recipe-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from "../recipe.model";
 
@@ -8,14 +8,19 @@ import { Recipe } from "../recipe.model";
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe("A Test Recipe", "This is simply a test", "https://images.themodernproper.com/billowy-turkey/production/posts/2020/Creamy-Tomato-Chicken-Skillet-11.jpg?w=1200&auto=compress%2Cformat&fit=crop&fp-x=0.5&fp-y=0.5&dm=1599768537&s=7f47312bc3ecd09e7a456c3c946e3872"),
-    new Recipe("A Test Recipe", "This is simply a test", "https://images.themodernproper.com/billowy-turkey/production/posts/2020/Creamy-Tomato-Chicken-Skillet-11.jpg?w=1200&auto=compress%2Cformat&fit=crop&fp-x=0.5&fp-y=0.5&dm=1599768537&s=7f47312bc3ecd09e7a456c3c946e3872")
+    new Recipe("Another Test Recipe", "This is simply a test", "https://images.themodernproper.com/billowy-turkey/production/posts/2020/Creamy-Tomato-Chicken-Skillet-11.jpg?w=1200&auto=compress%2Cformat&fit=crop&fp-x=0.5&fp-y=0.5&dm=1599768537&s=7f47312bc3ecd09e7a456c3c946e3872")
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
