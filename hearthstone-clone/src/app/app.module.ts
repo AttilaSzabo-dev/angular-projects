c08f8286ff07ff86365415b16082a37ff4dc6441
Attila Szabo <attila.szabo.vrc@gmail.com>
2021-04-13 22:21:53 +0200

    grid started

--- hearthstone-clone/src/app/app.module.ts
+++ hearthstone-clone/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardsHandComponent } from './cards-hand/cards-hand.component';
+import { CardItemComponent } from './cards-hand/card-item/card-item.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsHandComponent,
+    CardItemComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule
---
+++ hearthstone-clone/src/app/cards-hand/cards-hand.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-hand',
+  templateUrl: './cards-hand.component.html',
+  styleUrls: ['./cards-hand.component.scss']
+})
+export class CardsHandComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
