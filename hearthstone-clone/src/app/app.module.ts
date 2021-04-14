0d811297d20c0ad658df312b1f400c5c51db31a5
Attila Szabo <attila.szabo.vrc@gmail.com>
2021-04-14 14:00:26 +0200

    basic structure and components

--- hearthstone-clone/src/app/app.module.ts
+++ hearthstone-clone/src/app/app.module.ts
@@ -5,13 +5,21 @@ import { AppComponent } from './app.component';
 import { CardsHandComponent } from './cards-hand/cards-hand.component';
 import { CardItemComponent } from './cards-hand/card-item/card-item.component';
 import { HistoryComponent } from './history/history.component';
+import { ManaComponent } from './cards-hand/mana/mana.component';
+import { HeroComponent } from './hero/hero.component';
+import { HeroPowerComponent } from './hero/hero-power/hero-power.component';
+import { ArenaComponent } from './arena/arena.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardsHandComponent,
     CardItemComponent,
-    HistoryComponent
+    HistoryComponent,
+    ManaComponent,
+    HeroComponent,
+    HeroPowerComponent,
+    ArenaComponent
   ],
   imports: [
     BrowserModule
---
+++ hearthstone-clone/src/app/hero/hero-power/hero-power.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-power',
+  templateUrl: './hero-power.component.html',
+  styleUrls: ['./hero-power.component.scss']
+})
+export class HeroPowerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
