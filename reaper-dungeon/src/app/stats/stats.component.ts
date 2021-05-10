ff5a31f7e3bd0ed7a0b1a3609ea33676fdb973f5
Attila Szabo <attila.szabo.vrc@gmail.com>
2021-05-10 11:54:53 +0200

    stats design and string interpolation

--- reaper-dungeon/src/app/stats/stats.component.ts
+++ reaper-dungeon/src/app/stats/stats.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatsComponent implements OnInit {
   numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  goldAmount = 1000;
+  healthAmount = 100;
+  attackAmount = 1;
+  defenseAmount = 1;
 
   constructor() { }
 
